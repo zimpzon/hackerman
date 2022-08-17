2bc6d21a9ba691f7893cffacf5a7c5d7c065c63c
Peter Wennerstrøm Eriksen <PWE@commentor.dk>
2022-08-17 12:13:04 +0200

    backup

--- src/Components/ManualWorkButton/index.tsx
+++ src/Components/ManualWorkButton/index.tsx
@@ -6,30 +6,27 @@ interface ManualWorkButtonProps {
 
 function ManualWorkButton({ onClick }: ManualWorkButtonProps): JSX.Element {
   const animPushDown: Keyframe[] = [
-    { transform: 'scale(1)' },
-    { transform: 'scale(0.95)' },
-    { transform: 'scale(1)' },
+    { transform: "scale(1)" },
+    { transform: "scale(0.95)" },
+    { transform: "scale(1)" },
   ];
-  
+
   const animTiming: KeyframeAnimationOptions = {
     duration: 100,
     iterations: 1,
-    easing: 'ease-out'
-  }
+    easing: "ease-out",
+  };
 
   const click = (e: React.MouseEvent) => {
-    const element = e.currentTarget as HTMLElement
-    element.animate(animPushDown, animTiming)
+    const element = e.currentTarget as HTMLElement;
+    element.animate(animPushDown, animTiming);
 
-    onClick(e)
-  }
+    onClick(e);
+  };
 
   return (
     <>
-      <div id="manualWorkButton" onClick={click}>
-        Click, click, click...
-        <div id="manualWorkIncome"></div>
-      </div>
+      <div id="manualWorkButton" onClick={click}></div>
     </>
   );
 }
